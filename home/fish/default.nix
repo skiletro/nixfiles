41127e9b3e863bea0145ef9973a4b0ca675c2707
skiletro <19377854+skiletro@users.noreply.github.com>
2023-07-06 16:58:32 +0100

    fix: tab issue

--- home/fish/default.nix
+++ home/fish/default.nix
@@ -6,7 +6,7 @@
       interactiveShellInit = ''
         set fish_greeting # Disables the greeting
         direnv hook fish | source # 
-	      starship init fish | source # Enables starship
+        starship init fish | source # Enables starship
       '';
     };
 }
