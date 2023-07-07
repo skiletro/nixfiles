0b21dac657a0f06c4fb7a9742fc3b05a2535529f
skiletro <19377854+skiletro@users.noreply.github.com>
2023-07-07 19:21:33 +0100

    update: nvchad plugins

--- packages/nvchad/custom/plugins.lua
+++ packages/nvchad/custom/plugins.lua
@@ -1,8 +1,30 @@
 local plugins = {
-
+  
+  -- direnv, kinda its own thing?
   { "direnv/direnv.vim", lazy = false },
 
-  { "elkowar/yuck.vim", lazy = false}
+  -- language highlighting
+  { "elkowar/yuck.vim", lazy = false},
+
+  -- ui changes
+  { "rcarriga/nvim-notify" },
+  
+  { "stevearc/dressing.nvim", lazy = true },
+
+  { 
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+        inc_rename = true,
+      },
+    },
+  },
+
+  { "MunifTanjim/nui.nvim", lazy = true }
 
 }
 
