c0560be48ebda7513bb99c1c423f5d54f83bf2be
skiletro <19377854+skiletro@users.noreply.github.com>
2023-07-07 18:10:56 +0100

    update: nvchad plugins

--- packages/nvchad/custom/chadrc.lua
+++ packages/nvchad/custom/chadrc.lua
@@ -16,4 +16,6 @@ M.ui = {
   },
 }
 
+M.plugins = "custom.plugins"
+
 return M
---
+++ packages/nvchad/custom/plugins.lua
@@ -0,0 +1,9 @@
+local plugins = {
+
+  { "direnv/direnv.vim", lazy = false },
+
+  { "elkowar/yuck.vim", lazy = false}
+
+}
+
+return plugins
