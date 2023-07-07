139044d91a0303e94cceaa512435c220d6ee654f
skiletro <19377854+skiletro@users.noreply.github.com>
2023-07-07 18:02:15 +0100

    fix: oops

--- home/sway/default.nix
+++ home/sway/default.nix
@@ -2,18 +2,12 @@
 
 {
 
-  #services.xserver.enable = true;
-  #services.xserver.displayManager.sddm.enable = true;
-
-  wayland.windowManager.hyprland = {
+  wayland.windowManager.sway = {
     enable = true;
-
-    extraConfig = ''
-      env = WLR_NO_HARDWARE_CURSORS, 1
-      env = WLR_RENDERER_ALLOW_SOFTWARE, 1
-      bind = SUPER, Return, exec, kitty
-      exec-once = swaybg -c ff00aa
-    '';
+    config = rec {
+      modifier = "Mod4";
+      terminal = "foot";
+    };
   };
 
 }
