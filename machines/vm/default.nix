eab88cb6d996b5f66e27ee1a4d4fadfa8ff17a5c
skiletro <19377854+skiletro@users.noreply.github.com>
2023-07-07 15:47:38 +0100

    fix: get rid of unneeded plasma stuff

--- machines/vm/default.nix
+++ machines/vm/default.nix
@@ -51,6 +51,16 @@
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
+  environment.plasma5.excludePackages = with pkgs.libsForQt5; [
+    elisa
+    gwenview
+    okular
+    oxygen
+    khelpcenter
+    konsole
+    plasma-browser-integration
+    print-manager
+  ];
 
   # Configure keymap in X11
   services.xserver = {
