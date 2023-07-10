09d3a6002ccc811f768e3d1529683396cc050fe8
skiletro <19377854+skiletro@users.noreply.github.com>
2023-07-10 03:20:31 +0100

    update: finally gtk theme!!

--- home/default.nix
+++ home/default.nix
@@ -64,7 +64,6 @@
 
     # misc
     swayosd
-    catppuccin-papirus-folders
     catppuccin-gtk
     catppuccin-cursors
     
@@ -75,16 +74,17 @@
   ];
 
   gtk = {
+    enable = true;
+
     iconTheme.package = pkgs.catppuccin-papirus-folders.override {
       flavor = "mocha";
-      accent = "blue";
+      accent = "mauve";
     };
     iconTheme.name = "Papirus-Dark";
 
     theme.package = pkgs.catppuccin-gtk.override {
       size = "standard";
       variant = "mocha";
-
     };
     theme.name = "Catppuccin-Mocha-Standard-Blue-dark";
 
@@ -93,6 +93,14 @@
     cursorTheme.size = 24;
 
     font.name = "Iosevka Eos";
+  };
+
+  home.pointerCursor = {
+    package = pkgs.catppuccin-cursors.mochaDark;
+    name = "Catppuccin-Mocha-Dark-Cursors";
+    size = 24;
+    gtk.enable = true;
+    x11.enable = true;
   };
 
   home.stateVersion = "23.05";
