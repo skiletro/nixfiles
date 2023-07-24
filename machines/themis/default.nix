cf7ec6d23738221b59518c25d043c7db35a0d1c5
skiletro <19377854+skiletro@users.noreply.github.com>
2023-07-24 18:16:30 +0100

    systemd stuffs
    Former-commit-id: 5da37db70e71c4ecca90c64f536ffc5343d2e978

--- machines/themis/default.nix
+++ machines/themis/default.nix
@@ -12,6 +12,7 @@
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 5;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "themis";
