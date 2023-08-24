572bc784d102fa00ecc7c8f89fda3f185ac32bd1
skiletro <19377854+skiletro@users.noreply.github.com>
2023-08-25 00:07:36 +0100

    wlogout for power menu
    Former-commit-id: 8a14e6077e5344789a0961cab0949b3dde12286e

---
+++ home/wlogout/default.nix
@@ -0,0 +1,56 @@
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  xdg.configFile."wlogout/style.css".text = ''
+     * {
+       all: unset;
+       font-family: sans-serif;
+     }
+
+     window {
+       background-color: #1e1e2e;
+     }
+
+     button {
+       color: #313244;
+       font-size: 64px;
+       background-color: rgba(0,0,0,0);
+       outline-style: none;
+       margin: 5px;
+    }
+
+     button:focus, button:active, button:hover {
+       color: #cba7fa;
+       transition: ease 0.4s;
+     }
+  '';
+
+  xdg.configFile."wlogout/layout".text = ''
+    {
+      "label" : "lock",
+      "action" : "gtklock",
+      "text" : "󰌾",
+      "keybind" : ""
+    }
+    {
+      "label" : "logout",
+      "action" : "loginctl terminate-user $USER",
+      "text" : "󰗽",
+      "keybind" : ""
+    }
+    {
+      "label" : "shutdown",
+      "action" : "systemctl poweroff",
+      "text" : "󰐥",
+      "keybind" : ""
+    }
+    {
+      "label" : "reboot",
+      "action" : "systemctl reboot",
+      "text" : "󰑓",
+      "keybind" : ""
+    }
+  '';
+}
