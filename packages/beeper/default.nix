77b64f4451f47a175f95e5024a58d022b2608616
skiletro <19377854+skiletro@users.noreply.github.com>
2023-08-24 13:38:47 +0100

    update links
    Former-commit-id: 6ec49de440b31c70535e7aae40d06d7cb0851d15

--- packages/beeper/default.nix
+++ packages/beeper/default.nix
@@ -20,7 +20,7 @@ in
     version = "3.71.16";
 
     src = fetchurl {
-      url = "https://download.beeper.com/linux/appImage/x64";
+      url = "https://drive.usercontent.google.com/download?id=1zt__HvjKGxV6aUbV21fopfqrVffmpghL&export=download&authuser=0&confirm=t&uuid=95f6e06b-ab85-4f91-ace1-e2586f58ad86&at=APZUnTUEGxrCu5CfBrz74eA7uI2g:1692880446732";
       sha256 = "sha256-Ho5zFmhNzkOmzo/btV+qZfP2GGx5XvV/1JncEKlH4vc=";
     };
 
