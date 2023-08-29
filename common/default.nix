582cf09cb82947e6484f806fc31038cb9125b357
skiletro <19377854+skiletro@users.noreply.github.com>
2023-08-29 13:04:23 +0100

    wofi>fuzzel + cleanup
    Former-commit-id: f9573130f0e2d7844724411e14b4f675b6b50dac

--- common/default.nix
+++ common/default.nix
@@ -162,14 +162,10 @@
       substituters = [
         "https://hyprland.cachix.org"
         "https://nix-gaming.cachix.org"
-        "https://webcord.cachix.org"
-        "https://anyrun.cachix.org"
       ];
       trusted-public-keys = [
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-        "webcord.cachix.org-1:l555jqOZGHd2C9+vS8ccdh8FhqnGe8L78QrHNn+EFEs="
-        "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
       ];
     };
 
