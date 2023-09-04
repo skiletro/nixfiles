63385f42f7ddd21ff8f779dd46b0ebb4243a4f20
skiletro <19377854+skiletro@users.noreply.github.com>
2023-09-04 20:34:55 +0100

    📦 (beeper): added wayland flags by default ;)
    Former-commit-id: cb11c2b0409cec4df72c74af766be1da4bd7ad5e

--- packages/beeper/default.nix
+++ packages/beeper/default.nix
@@ -4,15 +4,16 @@
   makeDesktopItem,
 }: let
   pname = "beeper";
-  version = "3.71.16";
+  version = "3.74.4";
   name = "${pname}-${version}";
   src = fetchurl {
-    url = "https://drive.usercontent.google.com/download?id=1zt__HvjKGxV6aUbV21fopfqrVffmpghL&export=download&authuser=0&confirm=t&uuid=95f6e06b-ab85-4f91-ace1-e2586f58ad86&at=APZUnTUEGxrCu5CfBrz74eA7uI2g:1692880446732";
-    sha256 = "sha256-Ho5zFmhNzkOmzo/btV+qZfP2GGx5XvV/1JncEKlH4vc=";
+    url = "https://download.todesktop.com/2003241lzgn20jd/${pname}-${version}.AppImage";
+    hash = "sha512-HQWZcgikYvOd+M3I8QZQr5cwK9Itat+Ru6V3wu55M0EEwbUiQSJPZFLbsW+cXNzqw3dFBaF00H5ZGe5VnM0+YA==";
+    # Get hashes from https://download.todesktop.com/2003241lzgn20jd/latest-linux.yml
   };
   desktop-file = makeDesktopItem rec {
     name = "Beeper";
-    exec = "beeper";
+    exec = "beeper --default-frame --enable-features=UseOzonePlatform --ozone-platform=wayland";
     icon = "beeper";
     comment = "Unified Desktop";
     desktopName = "Beeper";
@@ -35,6 +36,5 @@ in
       ln -s ${desktop-file}/share/applications/* $out/share/applications
 
       install -m 444 -D ${appimageContents}/${pname}.png $out/share/icons/hicolor/512x512/apps/${pname}.png
-
     '';
   }
