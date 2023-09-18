44f982e2672e1f909dd0ee9a6d974d2b22f34c92
skiletro <19377854+skiletro@users.noreply.github.com>
2023-09-18 13:53:08 +0100

    🔧 (term): alacritty->wezterm (bettr font rendring)

---
+++ home/wezterm/default.nix
@@ -0,0 +1,24 @@
+{pkgs, ...}: {
+  programs.wezterm = {
+    enable = true;
+    extraConfig = ''
+       local wezterm = require "wezterm"
+
+       return {
+         font = wezterm.font "Iosevka Comfy",
+         font_size = 11,
+         color_scheme = "Catppuccin Mocha",
+         enable_scroll_bar = false,
+         enable_tab_bar = false,
+         scrollback_lines = 10000,
+         window_padding = {
+           left = 30,
+           right = 30,
+           top = 30,
+           bottom = 30,
+         },
+         check_for_updates = false,
+      }
+    '';
+  };
+}
