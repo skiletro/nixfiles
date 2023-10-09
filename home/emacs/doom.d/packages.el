9ad9dfb978b8eba8a8c1e91f032391b039aae377
skiletro <19377854+skiletro@users.noreply.github.com>
2023-10-09 14:29:07 +0100

    (emacs): update configs
    Former-commit-id: 024df4f68367adc7dad36f7de217dfb65dce736a

--- home/emacs/doom.d/packages.el
+++ home/emacs/doom.d/packages.el
@@ -52,3 +52,4 @@
 (package! catppuccin-theme)
 (package! yuck-mode) ; editing yuck
 (package! rainbow-mode) ; preview colours indocument
+(package! org-fragtog) ; auto toggle latex fragment previews
