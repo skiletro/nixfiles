6895f6525f264576a39450e2d4012e442ca1439d
skiletro <19377854+skiletro@users.noreply.github.com>
2023-10-18 17:17:45 +0100

    🔧 (emacs): commented out onChange command

--- home/emacs/default.nix
+++ home/emacs/default.nix
@@ -9,6 +9,6 @@
     target = "../.doom.d";
     source = ./doom.d;
     recursive = true;
-    onChange = "${pkgs.libnotify}/bin/notify-send -u critical 'DOOM EMACS' 'Changed detected when rebuilding system. Please make sure to run `doom sync`!'";
+    #onChange = "${pkgs.libnotify}/bin/notify-send -u critical 'DOOM EMACS' 'Changed detected when rebuilding system. Please make sure to run `doom sync`!'";
   };
 }
