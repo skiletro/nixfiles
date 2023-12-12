747fe48e522de54218c32aa018a03a3507b4b17d
skiletro <19377854+skiletro@users.noreply.github.com>
2023-12-12 03:02:20 +0000

    gtklock -> swaylock

--- home/apps/wlogout/default.nix
+++ home/apps/wlogout/default.nix
@@ -1,4 +1,6 @@
-{...}: {
+{pkgs, ...}: {
+  home.packages = [pkgs.wlogout];
+
   xdg.configFile."wlogout/style.css".text = ''
      * {
        all: unset;
@@ -23,10 +25,44 @@
      }
   '';
 
-  xdg.configFile."wlogout/layout".text = ''
+  xdg.configFile."wlogout/layout".text = let
+    swaylock = pkgs.writeShellScript "swaylock-script" ''
+      ${pkgs.swaylock-effects}/bin/swaylock \
+        --screenshots \
+        --clock \
+        --text-color cba6f7 \
+        --text-ver-color cba6f7 \
+        --text-clear-color cba6f7 \
+        --text-wrong-color cba6f7 \
+      	--indicator \
+      	--indicator-radius 100 \
+      	--indicator-thickness 7 \
+      	--effect-blur 7x5 \
+      	--effect-vignette 0.5:0.5 \
+      	--ring-color 1e1e2e \
+        --ring-ver-color 1e1e2e \
+        --ring-clear-color cba6f7 \
+        --ring-wrong-color f38ba8 \
+        --bs-hl-color f38ba8 \
+      	--key-hl-color cba6f7 \
+      	--line-color 00000000 \
+        --line-ver-color 00000000 \
+        --line-clear-color 00000000 \
+        --line-wrong-color 00000000 \
+        --inside-color 00000088 \
+        --inside-ver-color 00000088 \
+        --inside-clear-color 00000088 \
+        --inside-wrong-color 00000088 \
+        --separator-color 00000000 \
+      	--fade-in 0 \
+        --timestr "%I:%M%p" \
+        --datestr "%b %d, %Y" \
+        --ignore-empty-password
+    '';
+  in ''
     {
       "label" : "lock",
-      "action" : "gtklock",
+      "action" : "${swaylock.outPath}",
       "text" : "󰌾",
       "keybind" : ""
     }
