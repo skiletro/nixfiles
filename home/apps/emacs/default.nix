f9d7cf3f6f1b9c3a2d87035e566c5b06abffa2f7
skiletro <19377854+skiletro@users.noreply.github.com>
2023-12-13 01:39:42 +0000

    (spicetify): refactor

--- home/apps/emacs/default.nix
+++ home/apps/emacs/default.nix
@@ -1,8 +1,10 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  emacs-pkg = pkgs.emacs29-pgtk;
+in {
   # For doom emacs to work, you'll need to clone the repo as per the instructions in the doomemacs/doomemacs repo
   programs.emacs = {
     enable = true;
-    package = pkgs.emacs29-pgtk;
+    package = emacs-pkg;
     extraPackages = epkgs: [
       epkgs.vterm
     ];
@@ -15,16 +17,26 @@
     #onChange = "${pkgs.libnotify}/bin/notify-send -u critical 'DOOM EMACS' 'Changed detected when rebuilding system. Please make sure to run `doom sync`!'";
   };
 
-  home.packages = with pkgs; [
-    rust-analyzer #rust lsp
-    nodePackages.bash-language-server #sh and bash
-    zls #zig lsp
-    rnix-lsp #nix lsp
-    ccls # c and c++ lsp
-    texlive.combined.scheme-medium #latex
-    pandoc #document conversion
-    gnuplot
-    graphviz #org roam graph generation
-    nodePackages_latest.pyright # python lsp
-  ];
+  home.packages = let
+    magit-script = pkgs.writeShellScriptBin "magit" ''
+      if [[ -d "./.git" ]]; then
+        ${emacs-pkg}/bin/emacs -nw --eval "(progn (magit-status) (delete-other-windows))"
+      else
+        echo "Err: Not a git repo" >>/dev/stderr
+      fi
+    '';
+  in
+    with pkgs; [
+      magit-script #^^
+      rust-analyzer #rust lsp
+      nodePackages.bash-language-server #sh and bash
+      zls #zig lsp
+      rnix-lsp #nix lsp
+      ccls # c and c++ lsp
+      texlive.combined.scheme-medium #latex
+      pandoc #document conversion
+      gnuplot
+      graphviz #org roam graph generation
+      nodePackages_latest.pyright # python lsp
+    ];
 }
