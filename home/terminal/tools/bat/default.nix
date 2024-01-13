aafa7a51d5a892edd1f49079d7db0511dd04efc5
skiletro <19377854+skiletro@users.noreply.github.com>
2024-01-13 21:24:25 +0000

    (bat): add catppuccin theme

--- home/terminal/tools/bat/default.nix
+++ home/terminal/tools/bat/default.nix
@@ -1,9 +1,14 @@
-{...}: {
+{pkgs, ...}: {
   programs.bat = {
     enable = true;
     config = {
-      theme = "base16-256";
+      theme = "catppuccin-mocha";
     };
+  };
+
+  xdg.configFile."bat/themes/catppuccin-mocha.tmTheme".source = pkgs.fetchurl {
+    url = "https://raw.g5ithubusercontent.com/catppuccin/bat/main/Catppuccin-mocha.tmTheme";
+    sha256 = "1z434yxjq95bbfs9lrhcy2y234k34hhj5frwmgmni6j8cqj0vi58";
   };
 
   programs.fish.shellAbbrs = {
