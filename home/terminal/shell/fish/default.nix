f1741cb5835901ea85b468ad7b34fddcff424b34
skiletro <19377854+skiletro@users.noreply.github.com>
2024-01-15 03:16:41 +0000

    (fish): add flake cd shortcut

--- home/terminal/shell/fish/default.nix
+++ home/terminal/shell/fish/default.nix
@@ -5,5 +5,8 @@
       set fish_greeting # Disables the greeting
         fish_add_path /home/jamie/.config/emacs/bin # for doom emacs ;p
     '';
+    shellAbbrs = {
+      n = "cd ~/.nix_config/";
+    };
   };
 }
