fa9073b1dd08137e21abe77d1e94768fb5a559a6
skiletro <19377854+skiletro@users.noreply.github.com>
2024-05-23 04:59:28 +0100

    (emacs): doom emacs -> hand rolled config
    Former-commit-id: 694e3f5a864b1004a2ece34002ba5686f89365cc

--- home/graphical/apps/emacs/default.nix
+++ home/graphical/apps/emacs/default.nix
@@ -1,42 +1,17 @@
-{pkgs, ...}: let
-  emacs-pkg = pkgs.emacs29-pgtk;
-in {
-  # For doom emacs to work, you'll need to clone the repo as per the instructions in the doomemacs/doomemacs repo
-  programs.emacs = {
-    enable = true;
-    package = emacs-pkg;
-    extraPackages = epkgs: [
-      epkgs.vterm
-    ];
+{pkgs, ...}: {
+  config = let
+    emacs-pkg = pkgs.emacs29-pgtk;
+  in {
+    programs.emacs = {
+      enable = true;
+      package = pkgs.emacsWithPackagesFromUsePackage {
+        package = emacs-pkg;
+        config = ./config.org;
+        defaultInitFile = true;
+        alwaysTangle = true;
+        extraEmacsPackages = epkgs: [epkgs.use-package];
+        alwaysEnsure = true;
+      };
+    };
   };
-
-  xdg.configFile.".doom.d" = {
-    target = "../.doom.d";
-    source = ./doom.d;
-    recursive = true;
-    #onChange = "${pkgs.libnotify}/bin/notify-send -u critical 'DOOM EMACS' 'Changed detected when rebuilding system. Please make sure to run `doom sync`!'";
-  };
-
-  home.packages = let
-    magit-script = pkgs.writeShellScriptBin "magit" ''
-      if [[ -d "./.git" ]]; then
-        ${emacs-pkg}/bin/emacs -nw --eval "(progn (magit-status) (delete-other-windows))"
-      else
-        echo "Err: Not a git repo" >>/dev/stderr
-      fi
-    '';
-  in
-    with pkgs; [
-      magit-script #^^
-      ispell # spellchecking
-      rust-analyzer #rust lsp
-      nodePackages.bash-language-server #sh and bash
-      zls #zig lsp
-      #rnix-lsp #nix lsp
-      ccls # c and c++ lsp
-      pandoc #document conversion
-      gnuplot
-      graphviz #org roam graph generation
-      nodePackages_latest.pyright # python lsp
-    ];
 }
