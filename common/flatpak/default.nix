007a05b31c8da5c1a8d0ccd0524c435c79839323
skiletro <19377854+skiletro@users.noreply.github.com>
2024-06-20 03:05:24 +0100

    (wsl): fix some typos and mistakes
    Former-commit-id: dd75869909d0375d64ef829702f6b2885cfdb52b

--- common/flatpak/default.nix
+++ common/flatpak/default.nix
@@ -1,31 +1,37 @@
-{...}: {
-  services.flatpak = {
-    enable = true;
-    update.auto = {
+{
+  lib,
+  config,
+  ...
+}: {
+  config = lib.mkIf config.userConfig.graphical.enable {
+    services.flatpak = {
       enable = true;
-      onCalendar = "weekly";
-    };
-    uninstallUnmanaged = true;
-    #packages = [];
-    overrides = {
-      global = {
-        # Force Wayland by default
-        Context.sockets = ["wayland" "!x11" "!fallback-x11"];
+      update.auto = {
+        enable = true;
+        onCalendar = "weekly";
+      };
+      uninstallUnmanaged = true;
+      #packages = [];
+      overrides = {
+        global = {
+          # Force Wayland by default
+          Context.sockets = ["wayland" "!x11" "!fallback-x11"];
 
-        Environment = {
-          # Fix un-themed cursor in some Wayland apps
-          XCURSOR_PATH = "/run/host/user-share/icons:/run/host/share/icons";
+          Environment = {
+            # Fix un-themed cursor in some Wayland apps
+            XCURSOR_PATH = "/run/host/user-share/icons:/run/host/share/icons";
 
-          # Force correct theme for some GTK apps
-          #GTK_THEME = "Adwaita:dark"; #TODO: Do some nix fuckery to get this value updating from elsewhere in the config
+            # Force correct theme for some GTK apps
+            #GTK_THEME = "Adwaita:dark"; #TODO: Do some nix fuckery to get this value updating from elsewhere in the config
+          };
         };
       };
     };
-  };
 
-  # Add flatpak installation location to the path
-  # This allows apps installed as a flatpak to be visible in any runners like rofi
-  environment.sessionVariables.PATH = [
-    "/var/lib/flatpak/exports/share/applications/"
-  ];
+    # Add flatpak installation location to the path
+    # This allows apps installed as a flatpak to be visible in any runners like rofi
+    environment.sessionVariables.PATH = [
+      "/var/lib/flatpak/exports/share/applications/"
+    ];
+  };
 }
--- hosts/wsl/default.nix
+++ hosts/wsl/default.nix
@@ -1,10 +1,17 @@
 {...}: {
   userConfig = {
-    graphical = false;
-    graphical.emacs = true;
+    graphical = {
+      enable = false;
+      emacs.enable = true;
+    };
   };
 
-  wsl.enable = true;
+  wsl = {
+    enable = true;
+    defaultUser = "jamie";
+    useWindowsDriver = true;
+    wslConf.automount.enabled = true;
+  };
 
   system.stateVersion = "24.11";
 }
