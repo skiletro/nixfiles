95138f14590564446157496c38acfd199143bc13
skiletro <19377854+skiletro@users.noreply.github.com>
2024-06-23 20:11:27 +0100

    (gnome): add gnome and gdm

--- common/greeters/default.nix
+++ common/greeters/default.nix
@@ -1,5 +1,6 @@
 {lib, ...}: {
   imports = [
+    ./gdm.nix
     ./greetd.nix
     ./sddm.nix
   ];
@@ -8,7 +9,7 @@
     userConfig.greeter = {
       enable = lib.mkEnableOption "Greeter";
       type = lib.mkOption {
-        type = lib.types.enum ["sddm" "greetd"];
+        type = lib.types.enum ["gdm" "greetd" "sddm"];
         default = "sddm";
         description = "specify the display manager greeter to use";
       };
