e35285d55f7aed4e7cb4c91bcae2845074c6daf3
skiletro <19377854+skiletro@users.noreply.github.com>
2024-06-24 21:21:58 +0100

    (stylix): add stylix to handle theming
    Former-commit-id: ff7d41f8f7e3bceb216f04705ab1696d10563ad4

--- home/graphical/terminalemus/kitty/default.nix
+++ home/graphical/terminalemus/kitty/default.nix
@@ -6,9 +6,7 @@
   config = lib.mkIf (osConfig.userConfig.desktop.terminalEmulator == "kitty") {
     programs.kitty = {
       enable = true;
-      theme = "Catppuccin-Mocha";
       settings = {
-        font_family = "Iosevka Comfy";
         window_padding_width = 10;
       };
     };
