c0181538c542becf3673eeabd84fbf870f1c6671
skiletro <19377854+skiletro@users.noreply.github.com>
2024-07-20 14:27:40 +0100

    (virtualisation): add podman properly

--- common/virtualisation/default.nix
+++ common/virtualisation/default.nix
@@ -12,8 +12,18 @@
   };
 
   config = lib.mkIf config.userConfig.virtualisation.enable {
+    # Virt Manager
     virtualisation.libvirtd.enable = true;
     programs.virt-manager.enable = true;
+
+    # Podman (drop-in replacement for Docker)
+    virtualisation.podman = {
+      enable = true;
+      dockerCompat = true; # `docker` alias for podman - drop-in replacement
+      defaultNetwork.settings.dns_enabled = lib.mkDefault true; # Required for containers under podman-compose to be able to talk to each other.
+    };
+
+    # Misc Packages
     environment.systemPackages = let
       qemu-eufi = pkgs.writeShellScriptBin "qemu-system-x86_64-uefi" ''
         qemu-system-x86_64 \
@@ -25,9 +35,11 @@
         qemu
         qemu-eufi
         virtio-win
-        quickemu # quickly create and run optimised vms
-        distrobox
-        podman
+        quickemu # Quickly create and run optimised vms
+        distrobox # Tightly integrated Podman containers
+        podman-tui
+        podman-compose
+        podman-desktop
       ];
   };
 }
