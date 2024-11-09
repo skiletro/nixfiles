be50175f7f04f842211d61f8ef83fe37c3d50616
skiletro <19377854+skiletro@users.noreply.github.com>
2024-11-09 15:30:10 +0000

    (flake): remove dead code

--- home/programs/bat/default.nix
+++ home/programs/bat/default.nix
@@ -1,9 +1,4 @@
-{
-  pkgs,
-  lib,
-  osConfig,
-  ...
-}: {
+{...}: {
   config = {
     programs.bat.enable = true;
 
--- packages/default.nix
+++
@@ -1,4 +0,0 @@
-self: super: {
-  #beeper = super.callPackage ./beeper {};
-  #lutgen = super.callPackage ./lutgen {};
-}
