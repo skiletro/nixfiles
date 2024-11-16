6be5cda0f59abb962c73d1df9af7b20f1e1a6852
skiletro <19377854+skiletro@users.noreply.github.com>
2024-11-16 19:14:36 +0000

    (flake): replace just with devshell

--- home/services/fish/default.nix
+++ home/services/fish/default.nix
@@ -3,13 +3,9 @@
     enable = true;
     interactiveShellInit = ''
       set fish_greeting # Disables the greeting
-        fish_add_path /home/jamie/.config/emacs/bin # for doom emacs ;p
     '';
     shellAbbrs = {
       n = "cd ~/.nix_config/";
-      j = "just";
-      ju = "just update";
-      jr = "just run";
     };
   };
 }
