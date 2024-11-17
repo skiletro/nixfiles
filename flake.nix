c0e568f08ee818379623a72acbdebeef20cc7bcd
skiletro <19377854+skiletro@users.noreply.github.com>
2024-11-17 14:37:29 +0000

    (flake): small refactor Moves from direnv to nix-direnv, as it is a lot faster. Furthermore, makes some changes to some hosts, and reorders the flake inputs and outputs.
    Former-commit-id: 1dc3eafc888a75247e8f25b22cc67793575eb384

--- flake.nix
+++ flake.nix
@@ -2,40 +2,40 @@
   description = "nixfiles - Config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; # Unstable packages
-
-    home-manager.url = "github:nix-community/home-manager"; # Allows us to configure our home directory with Nix!
-    home-manager.inputs.nixpkgs.follows = "nixpkgs"; # This is so the HM flake uses our nixpkgs, instead of the nixpkgs commit in their repo
+    cursors.url = "github:lilleaila/nix-cursors"; # Recoloured cursors
+    cursors.inputs.nixpkgs.follows = "nixpkgs";
 
     devshell.url = "github:numtide/devshell";
     devshell.inputs.nixpkgs.follows = "nixpkgs";
 
-    zen-browser.url = "github:0xc000022070/zen-browser-flake"; # Best browser
-    zen-browser.inputs.nixpkgs.follows = "nixpkgs";
+    declarative-flatpak.url = "github:gmodena/nix-flatpak"; # Declare Flatpaks in this config!
 
-    stylix.url = "github:danth/stylix"; # Automatic styling
-    stylix.inputs.nixpkgs.follows = "nixpkgs";
+    emacs-overlay.url = "github:nix-community/emacs-overlay"; # Allows generating an emacs config using org mode + nix
+    emacs-overlay.inputs.nixpkgs.follows = "nixpkgs";
 
-    cursors.url = "github:lilleaila/nix-cursors"; # Recoloured cursors
-    cursors.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager"; # Allows us to configure our home directory with Nix!
+    home-manager.inputs.nixpkgs.follows = "nixpkgs"; # This is so the HM flake uses our nixpkgs, instead of the nixpkgs commit in their repo
+
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; # Unstable packages
+
+    nixvim.url = "github:nix-community/nixvim";
+    nixvim.inputs.nixpkgs.follows = "nixpkgs";
 
     nur.url = "github:nix-community/NUR"; # Nix User Repository; similar to the AUR.
 
     spicetify.url = "github:Gerg-L/spicetify-nix"; # Custom spotify theming
     spicetify.inputs.nixpkgs.follows = "nixpkgs";
 
-    emacs-overlay.url = "github:nix-community/emacs-overlay"; # Allows generating an emacs config using org mode + nix
-    emacs-overlay.inputs.nixpkgs.follows = "nixpkgs";
-
-    nixvim.url = "github:nix-community/nixvim";
-    nixvim.inputs.nixpkgs.follows = "nixpkgs";
-
-    declarative-flatpak.url = "github:gmodena/nix-flatpak"; # Declare Flatpaks in this config!
+    stylix.url = "github:danth/stylix"; # Automatic styling
+    stylix.inputs.nixpkgs.follows = "nixpkgs";
 
     vscode-extensions.url = "github:nix-community/nix-vscode-extensions"; # Declare VSC Extensions
     vscode-extensions.inputs.nixpkgs.follows = "nixpkgs";
 
     wsl.url = "github:nix-community/NixOS-WSL/main"; # WSL Support
+
+    zen-browser.url = "github:0xc000022070/zen-browser-flake"; # Best browser
+    zen-browser.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
@@ -68,6 +68,19 @@
       }
     ];
   in {
+    # Devshell - manage using devshell.toml file
+    devShells.${system}.default = let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [inputs.devshell.overlays.default];
+      };
+    in
+      pkgs.devshell.mkShell {imports = [(pkgs.devshell.importTOML ./devshell.toml)];}; # TODO: Probably a nicer way to express this
+
+    # Formatter
+    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
+
+    # System configurations
     nixosConfigurations = {
       eris = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs;};
@@ -80,15 +93,5 @@
         modules = commonModules ++ [./hosts/wsl];
       };
     };
-
-    devShells.${system}.default = let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [inputs.devshell.overlays.default];
-      };
-    in
-      pkgs.devshell.mkShell {imports = [(pkgs.devshell.importTOML ./devshell.toml)];}; # TODO: Probably a nicer way to express this
-
-    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
   };
 }
