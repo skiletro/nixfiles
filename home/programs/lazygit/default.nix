2ae8b3e7b145753380d78a247c7254ff00d297b0
skiletro <19377854+skiletro@users.noreply.github.com>
2024-12-22 00:09:49 +0000

    (feat): add abbreviation for lazygit

--- home/programs/lazygit/default.nix
+++ home/programs/lazygit/default.nix
@@ -5,6 +5,6 @@
 }: {
   config = lib.mkIf osConfig.userConfig.programs.lazygit.enable {
     programs.lazygit.enable = true;
-    # This file seems pretty pointless so far, but in case I want any custom settings in the future, it lets me add them a lot easier.
+    programs.fish.shellAbbrs.lg = "lazygit";
   };
 }
