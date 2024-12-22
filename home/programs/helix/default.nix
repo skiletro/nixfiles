a5bc6faa55f38adce3e0c5fc3b50f57c1cdaeb73
skiletro <19377854+skiletro@users.noreply.github.com>
2024-12-22 00:09:36 +0000

    (feat): moved lsp declaration outside of helix This is so that other programs, such as VSC, can use them
    Former-commit-id: 86feebdab19d44f09287feb260d5895d2f8b3436

--- home/programs/helix/default.nix
+++ home/programs/helix/default.nix
@@ -1,22 +1,11 @@
 {
   lib,
   osConfig,
-  pkgs,
   ...
 }: {
   config = lib.mkIf osConfig.userConfig.programs.helix.enable {
     programs.helix = {
       enable = true;
-      extraPackages = with pkgs; [
-        bash-language-server # Bash
-        gopls # Go
-        marksman # Markdown
-        nil # Nix
-        omnisharp-roslyn # C#
-        taplo # TOML
-        typescript-language-server # Javascript/Typescript
-        vscode-langservers-extracted # HTML/CSS/JSON/ESLint
-      ];
       settings = {
         editor = {
           cursor-shape = {
