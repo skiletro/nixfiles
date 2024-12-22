2fb66c22c849df25e46441ccd1b2ab62ab647249
skiletro <19377854+skiletro@users.noreply.github.com>
2024-12-22 14:38:07 +0000

    (feat): move noisetorch into separate module
    Former-commit-id: dbfc8d8860173be94e7edefd89e3a3c7735aee36

---
+++ modules/services/noisetorch/default.nix
@@ -0,0 +1,48 @@
+# Use https://github.com/noisetorch/NoiseTorch/wiki/Start-automatically-with-Systemd to find your deviceUnit and deviceID
+# TODO: Figure out why this isn't activating 100% of the time on boot.
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}: let
+  cfg = config.userConfig.services.noisetorch;
+  nt = "${pkgs.noisetorch}/bin/noisetorch";
+in {
+  options.userConfig.services.noisetorch = {
+    enable = lib.mkEnableOption ''NoiseTorch (+ setcap wrapper), a virtual microphone device with noise suppression, with a systemd service configured declaratively.'';
+    deviceUnit = lib.mkOption {
+      type = lib.types.str;
+      description = "Specify the microphone systemd device unit. This is done to prevent the NoiseTorch service from loading before the mic becomes available.";
+    };
+    deviceID = lib.mkOption {
+      type = lib.types.str;
+      description = "Specify the microphone device ID.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    security.wrappers.noisetorch = {
+      owner = "root";
+      group = "root";
+      capabilities = "cap_sys_resource=+ep";
+      source = nt;
+    };
+
+    systemd.user.services.noisetorch = {
+      enable = true;
+      description = "NoiseTorch Audio Cancelling";
+      wantedBy = ["default.target"];
+      requires = [cfg.deviceUnit];
+      after = ["pipewire.service" cfg.deviceUnit];
+      serviceConfig = {
+        Type = "simple";
+        RemainAfterExit = "yes";
+        ExecStart = ''${nt} -s ${cfg.deviceID} -i -o'';
+        ExecStop = ''${nt} -u'';
+        Restart = "on-failure";
+        RestartSec = 3;
+      };
+    };
+  };
+}
