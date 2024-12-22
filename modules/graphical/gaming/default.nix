f721de3f0d978d5a21d953b1ba5098e9ed5a7636
skiletro <19377854+skiletro@users.noreply.github.com>
2024-12-22 00:11:20 +0000

    (feat): add roblox client
    Former-commit-id: 416f094e73c5cd594c1355e41b922e50a4d253b6

--- modules/graphical/gaming/default.nix
+++ modules/graphical/gaming/default.nix
@@ -83,6 +83,10 @@
 
     services.flatpak.packages = [
       "sh.ppy.osu"
+      {
+        flatpakref = "https://sober.vinegarhq.org/sober.flatpakref";
+        sha256 = "1pj8y1xhiwgbnhrr3yr3ybpfis9slrl73i0b1lc9q89vhip6ym2l";
+      }
     ];
   };
 }
