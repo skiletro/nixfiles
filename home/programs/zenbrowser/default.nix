68aeff4db98ef49c0c772081dacc87f9c551e44d
skiletro <19377854+skiletro@users.noreply.github.com>
2024-12-24 18:47:49 +0000

    (chore): update zen browser input
    Former-commit-id: 03d242ed06e2aa241eeb8d50853108c4660a416c

--- home/programs/zenbrowser/default.nix
+++ home/programs/zenbrowser/default.nix
@@ -7,6 +7,6 @@
 }: {
   config = lib.mkIf osConfig.userConfig.programs.zenbrowser.enable {
     # This module seems very barebones at the moment, but it will allow for the browser to be configured a lot easier in the future.
-    home.packages = [inputs.zen-browser.packages.${pkgs.system}.specific];
+    home.packages = [inputs.zen-browser.packages.${pkgs.system}];
   };
 }
