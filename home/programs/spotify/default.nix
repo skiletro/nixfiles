790f675cbb7d498830e5a26e2db8a398ec49a626
skiletro <19377854+skiletro@users.noreply.github.com>
2024-12-27 15:01:38 +0000

    (fix): fix spotify theme looking weird
    Former-commit-id: 2b6b034fab82fb6d65daa20da069b14c4f7a21da

--- home/programs/spotify/default.nix
+++ home/programs/spotify/default.nix
@@ -8,6 +8,8 @@
   imports = [inputs.spicetify.homeManagerModules.default];
 
   config = lib.mkIf osConfig.userConfig.programs.spotify.enable {
+    stylix.targets.spicetify.enable = false; # The default spicetify theme colours kinda suck
+
     programs.spicetify = let
       spicetify = inputs.spicetify.legacyPackages.${pkgs.system};
     in {
@@ -21,6 +23,39 @@
         newReleases
         lyricsPlus
       ];
+
+      theme = {
+        name = "stylix";
+        src = pkgs.writeTextFile {
+          name = "color.ini";
+          destination = "/color.ini";
+          text = with osConfig.lib.stylix.colors; ''
+            [base]
+            text               = ${base05}
+            subtext            = ${base05}
+            main               = ${base00}
+            main-elevated      = ${base02}
+            highlight          = ${base02}
+            highlight-elevated = ${base03}
+            sidebar            = ${base01}
+            player             = ${base05}
+            card               = ${base00}
+            shadow             = ${base00}
+            selected-row       = ${base05}
+            button             = ${base0B}
+            button-active      = ${base05}
+            button-disabled    = ${base03}
+            tab-active         = ${base0B}
+            notification       = ${base00}
+            notification-error = ${base08}
+            equalizer          = ${base0B}
+            misc               = ${base00}
+          '';
+        };
+        # Sidebar configuration is incompatible with the default navigation bar
+        sidebarConfig = false;
+      };
+      colorScheme = "base";
     };
 
     # We are overriding the default .desktop file because of GNOME and their stupid CSDs.
