77f2b0c8f0b912900ba18e36da7392f0522f1960
skiletro <19377854+skiletro@users.noreply.github.com>
2025-01-15 21:33:15 +0000

    (refactor): konsole stylix

--- home/programs/konsole/default.nix
+++ home/programs/konsole/default.nix
@@ -7,108 +7,88 @@
   config = lib.mkIf (osConfig.userConfig.desktop.terminalEmulator == "konsole") {
     home.packages = with pkgs; [konsole];
 
-    # TODO: There *must* be a better way of doing this... this is horrendous...
-    xdg.dataFile."konsole/Stylix.colorscheme".text = with osConfig.lib.stylix.colors; ''
-      [Background]
-      Color=${base00-rgb-r}, ${base00-rgb-g}, ${base00-rgb-b}
+    xdg.dataFile."konsole/Stylix.profile".text = lib.generators.toINI {} {
+      General = {
+        Name = "Stylix";
+        Parent = "FALLBACK/";
+        TerminalCenter = false;
+        TerminalMargin = 4;
+      };
 
-      [BackgroundFaint]
-      Color=${base00-rgb-r}, ${base00-rgb-g}, ${base00-rgb-b}
+      Appearance = {
+        AntiAliasFonts = true;
+        ColorScheme = "Stylix";
+      };
 
-      [BackgroundIntense]
-      Color=${base03-rgb-r}, ${base03-rgb-g}, ${base03-rgb-b}
+      "Cursor Options".CursorShape = 1;
+      "Terminal Features".BlinkingCursorEnabled = true;
+    };
 
-      [Color0]
-      Color=${base00-rgb-r}, ${base00-rgb-g}, ${base00-rgb-b}
+    xdg.dataFile."konsole/Stylix.colorscheme".text = with osConfig.lib.stylix.colors; let
+      fmt = r: g: b: "${r}, ${g}, ${b}";
 
-      [Color0Faint]
-      Color=${base00-rgb-r}, ${base00-rgb-g}, ${base00-rgb-b}
+      foreground = fmt base05-rgb-r base05-rgb-g base05-rgb-b;
+      background = fmt base00-rgb-r base00-rgb-g base00-rgb-b;
+      background-lighter = fmt base03-rgb-r base03-rgb-g base03-rgb-b;
 
-      [Color0Intense]
-      Color=${base03-rgb-r}, ${base03-rgb-g}, ${base03-rgb-b}
+      red = fmt base08-rgb-r base08-rgb-g base08-rgb-b;
+      green = fmt base0B-rgb-r base0B-rgb-g base0B-rgb-b;
+      yellow = fmt base0A-rgb-r base0A-rgb-g base0A-rgb-b;
+      blue = fmt base0D-rgb-r base0D-rgb-g base0D-rgb-b;
+      purple = fmt base0E-rgb-r base0E-rgb-g base0D-rgb-b;
+      cyan = fmt base0C-rgb-r base0C-rgb-g base0C-rgb-b;
+    in
+      lib.generators.toINI {} {
+        Background.Color = background;
+        BackgroundFaint.Color = background;
+        BackgroundIntense.Color = background-lighter;
 
-      [Color1]
-      Color=${base08-rgb-r}, ${base08-rgb-g}, ${base08-rgb-b}
+        Color0.Color = background;
+        Color0Faint.Color = background;
+        Color0Intense.Color = background-lighter;
 
-      [Color1Faint]
-      Color=${base08-rgb-r}, ${base08-rgb-g}, ${base08-rgb-b}
+        Color1.Color = red;
+        Color1Faint.Color = red;
+        Color1Intense.Color = red;
 
-      [Color1Intense]
-      Color=${base08-rgb-r}, ${base08-rgb-g}, ${base08-rgb-b}
+        Color2.Color = green;
+        Color2Faint.Color = green;
+        Color2Intense.Color = green;
 
-      [Color2]
-      Color=${base0B-rgb-r}, ${base0B-rgb-g}, ${base0B-rgb-b}
+        Color3.Color = yellow;
+        Color3Faint.Color = yellow;
+        Color3Intense.Color = yellow;
 
-      [Color2Faint]
-      Color=${base0B-rgb-r}, ${base0B-rgb-g}, ${base0B-rgb-b}
+        Color4.Color = blue;
+        Color4Faint.Color = blue;
+        Color4Intense.Color = blue;
 
-      [Color2Intense]
-      Color=${base0B-rgb-r}, ${base0B-rgb-g}, ${base0B-rgb-b}
+        Color5.Color = purple;
+        Color5Faint.Color = purple;
+        Color5Intense.Color = purple;
 
-      [Color3]
-      Color=${base0A-rgb-r}, ${base0A-rgb-g}, ${base0A-rgb-b}
+        Color6.Color = cyan;
+        Color6Faint.Color = cyan;
+        Color6Intense.Color = cyan;
 
-      [Color3Faint]
-      Color=${base0A-rgb-r}, ${base0A-rgb-g}, ${base0A-rgb-b}
+        Color7.Color = foreground;
+        Color7Faint.Color = foreground;
+        Color7Intense.Color = foreground;
+        Foreground.Color = foreground;
+        ForegroundFaint.Color = foreground;
+        ForegroundIntense.Color = foreground;
 
-      [Color3Intense]
-      Color=${base0A-rgb-r}, ${base0A-rgb-g}, ${base0A-rgb-b}
-
-      [Color4]
-      Color=${base0D-rgb-r}, ${base0D-rgb-g}, ${base0D-rgb-b}
-
-      [Color4Faint]
-      Color=${base0D-rgb-r}, ${base0D-rgb-g}, ${base0D-rgb-b}
-
-      [Color4Intense]
-      Color=${base0D-rgb-r}, ${base0D-rgb-g}, ${base0D-rgb-b}
-
-      [Color5]
-      Color=${base0E-rgb-r}, ${base0E-rgb-g}, ${base0E-rgb-b}
-
-      [Color5Faint]
-      Color=${base0E-rgb-r}, ${base0E-rgb-g}, ${base0E-rgb-b}
-
-      [Color5Intense]
-      Color=${base0E-rgb-r}, ${base0E-rgb-g}, ${base0E-rgb-b}
-
-      [Color6]
-      Color=${base0C-rgb-r}, ${base0C-rgb-g}, ${base0C-rgb-b}
-
-      [Color6Faint]
-      Color=${base0C-rgb-r}, ${base0C-rgb-g}, ${base0C-rgb-b}
-
-      [Color6Intense]
-      Color=${base0C-rgb-r}, ${base0C-rgb-g}, ${base0C-rgb-b}
-
-      [Color7]
-      Color=${base05-rgb-r}, ${base05-rgb-g}, ${base05-rgb-b}
-
-      [Color7Faint]
-      Color=${base05-rgb-r}, ${base05-rgb-g}, ${base05-rgb-b}
-
-      [Color7Intense]
-      Color=${base05-rgb-r}, ${base05-rgb-g}, ${base05-rgb-b}
-
-      [Foreground]
-      Color=${base05-rgb-r}, ${base05-rgb-g}, ${base05-rgb-b}
-
-      [ForegroundFaint]
-      Color=${base05-rgb-r}, ${base05-rgb-g}, ${base05-rgb-b}
-
-      [ForegroundIntense]
-      Color=${base05-rgb-r}, ${base05-rgb-g}, ${base05-rgb-b}
-
-      [General]
-      Anchor=0.5,0.5
-      Blur=false
-      ColorRandomization=false
-      Description=Stylix
-      FillStyle=Tile
-      Opacity=1
-      Wallpaper=
-      WallpaperFlipType=NoFlip
-      WallpaperOpacity=1
-    '';
+        General = {
+          Anchor = "0.5,0.5";
+          Blur = false;
+          ColorRandomization = false;
+          Description = "Stylix";
+          FillStyle = "Tile";
+          Opacity = 1;
+          Wallpaper = "";
+          WallpaperFlipType = "NoFlip";
+          WallpaperOpacity = 1;
+        };
+      };
   };
 }
