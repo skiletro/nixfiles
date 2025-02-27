f03d49bbf640a9ff00648a6bb4943ebbc13e4e30
skiletro <19377854+skiletro@users.noreply.github.com>
2025-02-27 21:07:44 +0000

    feat: update icon theme and wallpaper

--- home/wms/plasma/default.nix
+++ home/wms/plasma/default.nix
@@ -1,9 +1,17 @@
 {
   lib,
   osConfig,
+  pkgs,
   ...
 }: {
   config = lib.mkIf (builtins.elem "plasma" osConfig.userConfig.desktop.environments) {
+    stylix.iconTheme = {
+      enable = true;
+      package = pkgs.papirus-icon-theme;
+      dark = "Papirus";
+      light = "Papirus";
+    };
+
     dconf = {
       enable = true;
       settings."org/gnome/desktop/wm/preferences".button-layout = ":minimize,maximize,close";
