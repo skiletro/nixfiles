107866f0e680aa309c088d651a3fc6e4541d6527
skiletro <19377854+skiletro@users.noreply.github.com>
2025-03-01 21:22:24 +0000

    fix: allow wireguard traffic to go through firewall
    Former-commit-id: 17283f1675fb31f1213ab16f5d5b0fb3b8ab2c9e

--- modules/default.nix
+++ modules/default.nix
@@ -18,8 +18,27 @@
   # Kernel
   boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_zen; # Performance kernel
 
-  # Enable networking
-  networking.networkmanager.enable = true;
+  # Networking
+  networking = {
+    networkmanager.enable = true;
+
+    # Adapt rpfilter to ignore WireGuard traffic - allows WireGuard to work!
+    firewall = let
+      port = "51820"; # WireGuard endpoint port
+    in {
+      # if packets are still dropped, they will show up in dmesg
+      logReversePathDrops = true;
+      # wireguard trips rpfilter up
+      extraCommands = ''
+        ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --sport ${port} -j RETURN
+        ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --dport ${port} -j RETURN
+      '';
+      extraStopCommands = ''
+        ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --sport ${port} -j RETURN || true
+        ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --dport ${port} -j RETURN || true
+      '';
+    };
+  };
 
   users.users.jamie = {
     isNormalUser = true;
