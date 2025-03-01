28cca82f162837d51919049ba689624d7e8e2c49
skiletro <19377854+skiletro@users.noreply.github.com>
2025-03-01 21:22:38 +0000

    fix(discord): replace canary with stable
    Former-commit-id: 317ece838013795bbefa4655da4c925d60ef5f56

--- home/programs/discord/default.nix
+++ home/programs/discord/default.nix
@@ -5,20 +5,11 @@
   ...
 }: {
   config = lib.mkIf osConfig.userConfig.programs.discord.enable {
-    home.packages = with pkgs; [discord-canary];
-
-    # Overriding default .desktop file because the default one doesn't recognise canary as part of the actual Discord app (gnome issue)
-    xdg.desktopEntries.discord-canary = {
-      name = "Discord Canary";
-      genericName = "All-in-one cross-platform voice and text chat for gamers";
-      icon = "discord-canary";
-      exec = "DiscordCanary";
-      mimeType = ["x-scheme-handler/discord"];
-      categories = ["Network" "InstantMessaging"];
-      settings = {
-        Version = "1.4";
-        StartupWMClass = "discord";
-      };
-    };
+    home.packages = [
+      # Remember to enable the Stylix theme in the Vencord settings!
+      (pkgs.discord.override {
+        withVencord = true;
+      })
+    ];
   };
 }
