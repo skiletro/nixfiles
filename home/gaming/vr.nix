79db1500c25fda5ff0bae416fb662a39e0651701
skiletro <19377854+skiletro@users.noreply.github.com>
2025-03-02 17:45:18 +0000

    fix(wivrn): replace flatpak with native package

--- home/gaming/vr.nix
+++ home/gaming/vr.nix
@@ -2,30 +2,31 @@
   lib,
   config,
   osConfig,
+  pkgs,
   ...
 }: {
   config = lib.mkIf osConfig.userConfig.gaming.vr.enable {
-    # # This assumes a WiVRn configuration
-    # xdg.configFile."openxr/1/active_runtime.json".source = "${wivrn}/share/openxr/1/openxr_wivrn.json";
+    # This assumes a WiVRn configuration
+    xdg.configFile."openxr/1/active_runtime.json".source = "${osConfig.services.wivrn.package}/share/openxr/1/openxr_wivrn.json";
 
-    # xdg.configFile."openvr/openvrpaths.vrpath".text = ''
-    #   {
-    #     "config" :
-    #     [
-    #       "${config.xdg.dataHome}/Steam/config"
-    #     ],
-    #     "external_drivers" : null,
-    #     "jsonid" : "vrpathreg",
-    #     "log" :
-    #     [
-    #       "${config.xdg.dataHome}/Steam/logs"
-    #     ],
-    #     "runtime" :
-    #     [
-    #       "${pkgs.opencomposite}/lib/opencomposite"
-    #     ],
-    #     "version" : 1
-    #   }
-    # '';
+    xdg.configFile."openvr/openvrpaths.vrpath".text = ''
+      {
+        "config" :
+        [
+          "${config.xdg.dataHome}/Steam/config"
+        ],
+        "external_drivers" : null,
+        "jsonid" : "vrpathreg",
+        "log" :
+        [
+          "${config.xdg.dataHome}/Steam/logs"
+        ],
+        "runtime" :
+        [
+          "${pkgs.opencomposite}/lib/opencomposite"
+        ],
+        "version" : 1
+      }
+    '';
   };
 }
