c6bfafb523ef46d43ddc6c22a7154641e0be1c8a
skiletro <19377854+skiletro@users.noreply.github.com>
2025-05-20 02:47:03 +0100

    feat(discord): disable vencord for now
    doesn't load with latest update

--- home/programs/discord/default.nix
+++ home/programs/discord/default.nix
@@ -8,7 +8,7 @@
     home.packages = [
       # Remember to enable the Stylix theme in the Vencord settings!
       (pkgs.discord.override {
-        withVencord = true;
+        #withVencord = true;
       })
     ];
   };
