afcef8e203d24cf2bcb62216cc57a32e1243b5f5
skiletro <19377854+skiletro@users.noreply.github.com>
2025-05-20 03:40:59 +0100

    feat(sunshine): add new mode + dynamically change resolution
    Former-commit-id: f62f0e92c2472b5f4b69f5f8ea028b6c133b1f4d

--- modules/graphical/gaming/gamestreaming.nix
+++ modules/graphical/gaming/gamestreaming.nix
@@ -16,21 +16,26 @@
         lib.lists.forEach [
           {
             name = "Desktop";
-            exclude-global-prep-cmd = "false";
             auto-detach = "true";
           }
           {
+            name = "Steam Big Picture";
+            detached = ["${lib.getExe' pkgs.util-linux "setsid"} ${lib.getExe pkgs.steam} steam://open/bigpicture"]; # TODO: This doesn't work all of the time.
+          }
+          {
             name = "Pegasus";
-            exclude-global-prep-cmd = "false";
             cmd = "kstart ${lib.getExe pkgs.pegasus-frontend}";
           }
         ] (attr:
           attr
           // {
-            prep-cmd = [
+            exclude-global-prep-cmd = "false";
+            prep-cmd = let
+              kscreen = lib.getExe pkgs.kdePackages.libkscreen;
+            in [
               {
-                do = ''${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output.DP-2.mode.1280x800@60''; # TODO: Don't hardcode this stuff
-                undo = ''${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output.DP-2.mode.3440x1440@165'';
+                do = "sh -c \"${kscreen} output.DP-2.mode.\${SUNSHINE_CLIENT_WIDTH}x\${SUNSHINE_CLIENT_HEIGHT}@\${SUNSHINE_CLIENT_FPS}\"";
+                undo = "${kscreen} output.DP-2.mode.3440x1440@165";
               }
             ];
           });
