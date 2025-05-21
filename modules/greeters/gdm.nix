80bbdb4c367e56531896c37847ee0a7b53ddd07d
skiletro <19377854+skiletro@users.noreply.github.com>
2025-05-21 01:26:04 +0100

    style: remove useless brackets
    Former-commit-id: de9aacc89544920033def1a5eb091f0882f22cc0

--- modules/greeters/gdm.nix
+++ modules/greeters/gdm.nix
@@ -3,7 +3,7 @@
   config,
   ...
 }: {
-  config = lib.mkIf ((config.userConfig.greeter.enable)
+  config = lib.mkIf (config.userConfig.greeter.enable
     && (config.userConfig.greeter.type == "gdm")) {
     services.xserver.displayManager.gdm = {
       enable = true;
