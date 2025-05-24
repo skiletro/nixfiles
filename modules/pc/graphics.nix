58129d257bbaa3fe87ad9fd8273d6b9054d90d7f
skiletro <19377854+skiletro@users.noreply.github.com>
2025-05-24 03:02:29 +0100

    refactor: makes graphics module easier to read
    Former-commit-id: 6869279797637dc3a93d0f7d407ec6fed99dac1f

--- modules/pc/graphics.nix
+++ modules/pc/graphics.nix
@@ -3,19 +3,25 @@
   config,
   ...
 }: let
+  inherit (lib) mkMerge mkIf;
   cfg = config.eos;
 in {
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
+  config = mkMerge [
+    {
+      hardware.graphics = {
+        enable = true;
+        enable32Bit = true;
+      };
+    }
+    (mkIf (cfg.system.gpu == "nvidia") {
+      hardware.nvidia = {
+        package = config.boot.kernelPackages.nvidiaPackages.beta;
+        modesetting.enable = true;
+        open = true;
+        nvidiaSettings = true;
+      };
 
-  hardware.nvidia = lib.mkIf (cfg.system.gpu == "nvidia") {
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
-    modesetting.enable = true;
-    open = true;
-    nvidiaSettings = true;
-  };
-
-  services.xserver.videoDrivers = lib.mkIf (cfg.system.gpu == "nvidia") ["nvidia"];
+      services.xserver.videoDrivers = ["nvidia"];
+    })
+  ];
 }
