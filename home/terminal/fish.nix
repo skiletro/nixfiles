f12d1303a83169babfd10e8b3f4c6126c45bce5b
skiletro <19377854+skiletro@users.noreply.github.com>
2025-06-16 16:46:13 +0100

    feat(fish): update helper functions for common tasks
    Former-commit-id: 0888a3e472f3033c8e60f94deb3ae201fa9e7003

--- home/terminal/fish.nix
+++ home/terminal/fish.nix
@@ -28,7 +28,27 @@
         "," =
           # fish
           ''
-            NIXPKGS_ALLOW_UNFREE=1 nix run "nixpkgs#$argv[1]" --impure -- $argv[2..-1]
+            if test (count $argv) -lt 1
+              echo "Usage: , <pkg> [extraArgs]"
+            else
+              NIXPKGS_ALLOW_UNFREE=1 nix run "nixpkgs#$argv[1]" --impure -- $argv[2..-1]
+            end
+          '';
+
+        ":" =
+          # fish
+          ''
+            if test (count $argv) -lt 1
+              echo "Usage: : <pkg1> [<pkg2> ... <pkgN>]"
+            else if test (count $argv) -eq 1
+              nix shell nixpkgs#$argv[1]
+            else
+              set args
+              for arg in $argv
+                set args $args "nixpkgs#$arg"
+              end
+              NIXPKGS_ALLOW_UNFREE=1 nix shell $args --impure
+            end
           '';
 
         "process_replays" =
