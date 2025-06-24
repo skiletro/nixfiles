ce68228793b1201157e21cf1a1ee148b8a102d6e
skiletro <19377854+skiletro@users.noreply.github.com>
2025-06-24 15:25:05 +0100

    fix(rclone): update script to check if it needs to make req to proton servers
    Former-commit-id: 1ba7edc8b44ba1e88fa9bd119f500728d12bc8e7

--- modules/pc/services/rclone.nix
+++ modules/pc/services/rclone.nix
@@ -17,13 +17,60 @@
       serviceConfig = {
         Type = "oneshot";
         User = config.eos.system.user;
-        ExecStart = lib.getExe (pkgs.writeShellScriptBin "rclone-documents-sync"
-          # sh
-          ''
-            ${lib.getExe pkgs.rclone} bisync --config="/run/agenix/rclone-protondrive.conf" -v --force --min-size 1b --max-lock 90m proton:Documents /home/${config.eos.system.user}/Documents/
-          '');
+        ExecStart = let
+          conf = "/run/agenix/rclone-protondrive.conf";
+        in
+          lib.getExe (pkgs.writers.writeFishBin "rclone-docs-sync"
+            # fish
+            ''
+              set HASH_FILE "/tmp/.rclone-protondrive-hash"
+              set REMOTE_DIR "Documents"
+              set SYNC_DIR "/home/${config.eos.system.user}/Documents"
+
+              set CURRENT_HASH (${lib.getExe pkgs.gnutar} fcP - $SYNC_DIR | md5sum)
+
+              function log -a val
+                  echo "$(date --rfc-3339 s) - RPROTON - $val"
+              end
+
+              function run_sync
+                  log "Running sync..."
+                  ${lib.getExe pkgs.rclone} bisync --config="${conf}" -v --force --min-size 1b proton:$REMOTE_DIR $SYNC_DIR
+              end
+
+              function create_hash_file
+                  log "Creating hash file..."
+                  echo $CURRENT_HASH >$HASH_FILE
+              end
+
+              function check_hash
+                  log "Checking directory hashes..."
+                  set prev_hash (cat $HASH_FILE)
+                  if test "$CURRENT_HASH" != "$prev_hash"
+                      log "Hash is not the same. Syncing..."
+                      run_sync
+                      create_hash_file
+                  else
+                      log "Hash is the same, no need to sync."
+                  end
+              end
+
+              function main
+                  log "Checking if previous hash file exists..."
+                  if test -f $HASH_FILE
+                      log "Hash file exists!"
+                      check_hash
+                  else
+                      log "Hash file does not exist."
+                      create_hash_file
+                      run_sync
+                  end
+              end
+
+              main
+            '');
       };
-      startAt = "*:0/30"; # Every 30 minutes
+      startAt = "*:0/15"; # Every 15 minutes
     };
   };
 }
